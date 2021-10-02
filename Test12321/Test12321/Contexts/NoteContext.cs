6422f4b5508b192092595b893f5cdd71220fdeca
Tatyana-Essentya <90097264+Prizrak433@users.noreply.github.com>
2021-10-02 17:56:47 +0300

    Lol

---
+++ Test12321/Test12321/Contexts/NoteContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Test12321.Models;
+
+namespace Test12321.Contexts
+{
+    public class NoteContext:DbContext
+    {
+        public NoteContext(DbContextOptions<NoteContext> option) : base(option)
+        {
+
+        }
+
+        public DbSet<Note> Notes { get; set; }
+
+    }
+}
---
+++ Test12321/Test12321/Models/Note.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Test12321.Models
+{
+    public class Note
+    {
+        public int id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime? ChangeDate { get; set; }
+
+    }
+}
